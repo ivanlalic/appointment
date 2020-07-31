ab6b0e45708d2edf0eb4753e4bf759528c860bf5
ivanlalic <ivanlalic@gmail.com>
2020-07-31 01:00:15 -0300

    Updates

---
+++ precache-manifest.52c5654db0f94e298c8c7ddcc808bb12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c509d5afa2fd386133728a4de19e4192",
+    "url": "/appointment/index.html"
+  },
+  {
+    "revision": "706815eb82458bc57725",
+    "url": "/appointment/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "7b90602570ca14de745f",
+    "url": "/appointment/static/js/2.2e2a9737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appointment/static/js/2.2e2a9737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706815eb82458bc57725",
+    "url": "/appointment/static/js/main.64115392.chunk.js"
+  },
+  {
+    "revision": "a06144b464a4ef648989",
+    "url": "/appointment/static/js/runtime-main.7b04eb90.js"
+  }
+]);
